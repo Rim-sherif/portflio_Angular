d1f28b15986075998dfe74ea130b4da34c35f798
Rim-sherif <120836789+Rim-sherif@users.noreply.github.com>
2025-01-24 21:01:57 +0200

    Angular Portflio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import { AboutComponent } from './about/about.component';
+import { SkilsComponent } from './skils/skils.component';
+import { PortfiloComponent } from './portfilo/portfilo.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,FooterComponent,AboutComponent,SkilsComponent,PortfiloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/portfilo/portfilo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfilo',
+  imports: [],
+  templateUrl: './portfilo.component.html',
+  styleUrl: './portfilo.component.css'
+})
+export class PortfiloComponent {
+
+}
